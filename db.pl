c848c7670957605957692922b88fb48b2961f2ca
Artur <larturl2004@yandex.ru>
2025-04-14 01:50:43 +0300

    Lab8.Task1

---
+++ db.pl
@@ -0,0 +1,88 @@
+%Task1_1
+man(anatoliy).
+man(dimitriy).
+man(vlad).
+man(kirill).
+man(mefodiy).
+woman(vladina).
+woman(galya).
+woman(sveta).
+woman(zoya).
+woman(katrin).
+dite(dimitriy, anatoliy).
+dite(dimitriy, galya).
+dite(vladina, anatoliy).
+dite(vladina, galya).
+dite(kirill, dimitriy).
+dite(mefodiy, dimitriy).
+dite(kirill, sveta).
+dite(mefodiy, sveta).
+dite(zoya, vlad).
+dite(zoya, vladina).
+dite(katrin, vlad).
+dite(katrin, vladina).
+
+
+%Task1_2 
+man :-
+    	man(X),
+    	write(X), nl,
+    	fail.
+man.
+
+woman :-
+    	woman(X),
+    	write(X), nl,
+    	fail.
+woman.
+
+
+%Task1_3
+parent(Parent, Child) :- dite(Child, Parent).
+
+children(X) :-
+	parent(X, Child),
+    	write(Child), nl,
+    	fail.
+children(_).
+
+
+%Task1_4
+mother(X, Y) :-
+    	woman(X),
+    	parent(X, Y).
+
+mother(X):-
+    	parent(M, X),
+    	woman(M),
+    	write(M), nl.
+
+
+%Task1_5
+brother(X, Y) :-
+	man(X),
+    	X \= Y,
+    	parent(P, X),
+   	parent(P, Y).
+
+brothers(X) :-
+	brother(Y, X),
+   	write(Y), nl,
+    	fail.
+brothers(_).
+
+%Task1_6
+b_s(X, Y):-
+	X \= Y,
+	parent(P,X),
+	arent(P,Y).
+       	
+b_s(X):-
+	b_s(X,Y),
+	write(Y), nl,
+	fail.
+b_s(_).
+
+
+
+
